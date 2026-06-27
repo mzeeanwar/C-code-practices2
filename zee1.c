39e241f80879bce4a965ba6074962ea4b391a52b
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-06-27 23:19:57 +0500

    Refactor to single-threaded process execution
    Refactored the threading implementation to use a single Process function instead of pthreads.

--- zee1.c
+++ zee1.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <pthread.h>
-int lock = 0;  // Shared lock variable
-int TestAndSet(int *target) {
-    int old = *target;
-    *target = 1;
-    return old;
+  int lock = 0;
+  int TestAndSet(int *target) {
+     int oldValue = *target;
+     *target = 1;
+     return oldValue; 
 }
-void* process(void* arg) {
-int id = *((int*)arg);
-while (TestAndSet(&lock) == 1);
-printf("Process %d is in the critical section\n", id);
-for (int i = 0; i < 1000000; i++);
-lock = 0;
-printf("Process %d exited critical section\n", id);
-return NULL;
-}
-int main() {
-    pthread_t t1, t2;
-    int id1 = 1, id2 = 2;
-
-    pthread_create(&t1, NULL, process, &id1);
-    pthread_create(&t2, NULL, process, &id2);
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-
-    return 0;
-}
+void Process(int id) {
+     while (TestAndSet(&lock)){}
+     printf("Process %d entered Critical Section\n", id);
+     printf("Process %d is executing...\n", id);
+     printf("Process %d leaving Critical Section\n\n", id);
+      lock = 0;
+ }
+  int main() {
+     Process(1);
+     Process(2);
+     return 0;
+ }
