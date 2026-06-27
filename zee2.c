f16674b03be6e62973288110059c813915386ccb
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-06-27 23:17:00 +0500

    Refactor process synchronization using custom semaphores
    Replaced pthreads and semaphores with custom semaphore functions for process synchronization.

--- zee2.c
+++ zee2.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-sem_t mutex;
-void* process(void* arg) {
-    int id = *((int*)arg);
-    sem_wait(&mutex);
-    printf("Process %d is in the critical section\n", id);
-    for (int i = 0; i < 1000000; i++);
-    sem_post(&mutex); 
-    printf("Process %d exited critical section\n", id);
-    return NULL;
-}
-
-int main() {
-    pthread_t t1, t2;
-    int id1 = 1, id2 = 2;
-    sem_init(&mutex, 0, 1);
-    pthread_create(&t1, NULL, process, &id1);
-    pthread_create(&t2, NULL, process, &id2);
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    sem_destroy(&mutex);
-    return 0;
+#include <stdio.h>  
+int semaphore = 1;
+void wait_sem() {
+     semaphore--; }
+ void signal_sem() {
+     semaphore++; 
+}  
+void Process1() {
+     wait_sem();
+      printf("Process 1 entered Critical Section\n");
+     printf("Process 1 is executing...\n");
+     printf("Process 1 leaving Critical Section\n\n");
+      signal_sem(); 
+}  
+void Process2() {
+     wait_sem();
+      printf("Process 2 entered Critical Section\n");
+     printf("Process 2 is executing...\n");
+     printf("Process 2 leaving Critical Section\n\n");
+      signal_sem();
+ }
+  int main() {
+     Process1();
+     Process2();
+     return 0;
 }
